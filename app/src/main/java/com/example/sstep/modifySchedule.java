f02610cf5bde2ae8883d02133a29a6478182ec20
kim <bbse3341@gmail.com>
2023-02-14 14:37:44 +0900

    직원정보, 일정 입력디자인

---
+++ app/src/main/java/com/example/sstep/modifySchedule.java
@@ -0,0 +1,14 @@
+package com.example.sstep;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class modifySchedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_schedule);
+    }
+}
