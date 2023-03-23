68f61bac72317119c2cee3d04b40b256e8c9fd4e
yuuuky02 <kim08146@naver.com>
2023-03-23 09:55:53 +0900

    camera & 디자인 수정

--- app/src/main/java/com/example/sstep/mypage/MyPage_Profile.java
+++ app/src/main/java/com/example/sstep/mypage/MyPage_Profile.java
@@ -3,21 +3,28 @@ package com.example.sstep.mypage;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.sstep.R;
 
 public class MyPage_Profile extends AppCompatActivity {
 
+    Button camerabtn;
     ImageButton backib;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage_profile);
+        ActivityCompat.requestPermissions(this, new String[]
+                {android.Manifest.permission.CAMERA}, MODE_PRIVATE);
         backib = findViewById(R.id.mypage_profile_backib);
+        camerabtn = findViewById(R.id.mypage_profile_camerabtn);
 
+        onCamera();
         // '뒤로가기' 선택 시
         backib.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +34,8 @@ public class MyPage_Profile extends AppCompatActivity {
                 finish();
             }
         });
+    }
+    public void onCamera(){
+        
     }
 }
