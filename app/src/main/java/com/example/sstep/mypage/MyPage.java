3a0676ab8efebbb10b90f07f6ea44ac14917aeb5
yuuuky02 <kim08146@naver.com>
2023-03-24 23:30:38 +0900

    mypage onclick 수정

--- app/src/main/java/com/example/sstep/mypage/MyPage.java
+++ app/src/main/java/com/example/sstep/mypage/MyPage.java
@@ -14,97 +14,78 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sstep.R;
 
-public class MyPage extends AppCompatActivity {
+public class MyPage extends AppCompatActivity implements View.OnClickListener {
 
-    LinearLayout mypage_HL1, mypage_HL3, mypage_HL4, mypage_HL5, mypage_HL6, mypage_HL7, mypage_HL8, mypage_HL9;
-    Dialog logoutdl, outdl;
+    LinearLayout alarmHL1, pwdHL2, changeHL3, messageHL4, askHL5, logoutHL6, dropHL7;
+    Button profileBtn;
+    Dialog logoutdl, dropdl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage);
 
-        mypage_HL1 = findViewById(R.id.mypage_HL1);mypage_HL3 = findViewById(R.id.mypage_HL3);
-        mypage_HL4 = findViewById(R.id.mypage_HL4);mypage_HL5 = findViewById(R.id.mypage_HL5);
-        mypage_HL6 = findViewById(R.id.mypage_HL6);mypage_HL7 = findViewById(R.id.mypage_HL7);
-        mypage_HL8 = findViewById(R.id.mypage_HL8);mypage_HL9 = findViewById(R.id.mypage_HL9);
+        alarmHL1 = findViewById(R.id.mypage_alarmHL1); alarmHL1.setOnClickListener(this);
+        pwdHL2 = findViewById(R.id.mypage_pwdHL2); pwdHL2.setOnClickListener(this);
+        changeHL3 = findViewById(R.id.mypage_changeHL3); changeHL3.setOnClickListener(this);
+        messageHL4 = findViewById(R.id.mypage_messageHL4); messageHL4.setOnClickListener(this);
+        askHL5 = findViewById(R.id.mypage_askHL5); askHL5.setOnClickListener(this);
+        logoutHL6 = findViewById(R.id.mypage_logoutHL6); logoutHL6.setOnClickListener(this);
+        dropHL7 = findViewById(R.id.mypage_dropHL7); dropHL7.setOnClickListener(this);
+        profileBtn = findViewById(R.id.mypage_profileBtn); profileBtn.setOnClickListener(this);
 
         logoutdl = new Dialog(MyPage.this); // Dialog 초기화
         logoutdl.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
         logoutdl.setContentView(R.layout.mypage_logoutdl); // xml 레이아웃 파일과 연결
 
-        outdl = new Dialog(MyPage.this); // Dialog 초기화
-        outdl.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
-        outdl.setContentView(R.layout.mypage_outdl); // xml 레이아웃 파일과 연결
+        dropdl = new Dialog(MyPage.this); // Dialog 초기화
+        dropdl.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
+        dropdl.setContentView(R.layout.mypage_outdl); // xml 레이아웃 파일과 연결
 
+    }
 
-        // '프로필' 선택 시
-        mypage_HL1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MyPage_Profile.class);
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()){
+            case R.id.mypage_profileBtn: // 프로필 수정
+                intent = new Intent(getApplicationContext(), MyPage_Profile.class);
                 startActivity(intent);
                 finish();
-            }
-        });
-        // '알림설정' 선택 시
-        mypage_HL3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MyPage_Alarm.class);
+                break;
+            case R.id.mypage_alarmHL1: // 알림 설정
+                intent = new Intent(getApplicationContext(), MyPage_Alarm.class);
                 startActivity(intent);
                 finish();
-            }
-        });
-        // '비밀번호 변경' 선택 시
-        mypage_HL4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MyPage_Pwd.class);
+                break;
+            case R.id.mypage_pwdHL2: // 비밀번호 변경
+                intent = new Intent(getApplicationContext(), MyPage_Pwd.class);
                 startActivity(intent);
                 finish();
-            }
-        });
-        // '사업주-직원 전환' 선택 시
-        mypage_HL5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MyPage_Change.class);
+                break;
+            case R.id.mypage_changeHL3: // 사업주-직원 전환
+                intent = new Intent(getApplicationContext(), MyPage_Change.class);
                 startActivity(intent);
                 finish();
-            }
-        });
-        // '문자 수신 설정' 선택 시
-        mypage_HL6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MyPage_Message.class);
+                break;
+            case R.id.mypage_messageHL4: // 문자 수신 설정
+                intent = new Intent(getApplicationContext(), MyPage_Message.class);
                 startActivity(intent);
                 finish();
-            }
-        });
-        // '문의하기' 선택 시
-        mypage_HL7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MyPage_Ask.class);
+                break;
+            case R.id.mypage_askHL5: // 문의하기
+                intent = new Intent(getApplicationContext(), MyPage_Ask.class);
                 startActivity(intent);
                 finish();
-            }
-        });
-        // '로그아웃' 선택 시
-        mypage_HL8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.mypage_logoutHL6: // 로그아웃
                 showlogoutDl();
-            }
-        });
-        // '탈퇴하기' 선택 시
-        mypage_HL9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showoutDl();
-            }
-        });
+                break;
+            case R.id.mypage_dropHL7: // 탈퇴하기
+                showdropDl();
+                break;
+            default:
+                break;
+        }
     }
 
     // '로그아웃 dialog' 띄우기
@@ -131,24 +112,24 @@ public class MyPage extends AppCompatActivity {
     }
 
     // '탈퇴하기 dialog' 띄우기
-    public void showoutDl() {
-        outdl.show();
-        outdl.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+    public void showdropDl() {
+        dropdl.show();
+        dropdl.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         Button outdl_canclebtn1, outdl_okbtn2;
-        outdl_canclebtn1 = outdl.findViewById(R.id.mypage_outdl_canclebtn1); outdl_okbtn2 = outdl.findViewById(R.id.mypage_outdl_okbtn2);
+        outdl_canclebtn1 = dropdl.findViewById(R.id.mypage_outdl_canclebtn1); outdl_okbtn2 = dropdl.findViewById(R.id.mypage_outdl_okbtn2);
 
         // '탈퇴하기 dialog' _ 취소 버튼 클릭 시
         outdl_canclebtn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                outdl.dismiss();
+                dropdl.dismiss();
             }
         });
         // '탈퇴하기 dialog' _ 탈퇴하기 버튼 클릭 시
         outdl_okbtn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                outdl.dismiss();
+                dropdl.dismiss();
             }
         });
     }
--- app/src/main/java/com/example/sstep/mypage/MyPage_Profile.java
+++ app/src/main/java/com/example/sstep/mypage/MyPage_Profile.java
@@ -1,6 +1,7 @@
 package com.example.sstep.mypage;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.telephony.PhoneNumberFormattingTextWatcher;
@@ -8,8 +9,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -18,9 +21,10 @@ import com.example.sstep.R;
 public class MyPage_Profile extends AppCompatActivity implements View.OnClickListener{
 
     EditText nameEt, phoneEt, emailEt;
-    ImageButton cameraIb;
-    ImageButton backib;
+    ImageButton backib, cameraIb;
+    ImageView profileIv;
     Intent intent;
+    private static final int REQ_CODE_SELECT_CAMERA = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +33,7 @@ public class MyPage_Profile extends AppCompatActivity implements View.OnClickLis
         nameEt = findViewById(R.id.mypage_profile_nameEt);
         phoneEt = findViewById(R.id.mypage_profile_phoneEt);
         emailEt = findViewById(R.id.mypage_profile_emailEt);
+        profileIv = findViewById(R.id.mypage_profile_profileIv);
 
         backib = findViewById(R.id.mypage_profile_backib);
         backib.setOnClickListener(this);
@@ -53,10 +58,27 @@ public class MyPage_Profile extends AppCompatActivity implements View.OnClickLis
                 break;
             case R.id.mypage_profile_cameraIb: // '카메라'
                 intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 30);
+                startActivityForResult(intent, REQ_CODE_SELECT_CAMERA);
                 break;
             default:
                 break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // 카메라 촬영을 하면 이미지뷰에 사진 삽입
+        if(requestCode == REQ_CODE_SELECT_CAMERA && resultCode == RESULT_OK) {
+            // Bundle로 데이터를 입력
+            Bundle extras = data.getExtras();
+
+            // Bitmap으로 컨버전
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            // 이미지뷰에 Bitmap으로 이미지를 입력
+            profileIv.setImageBitmap(imageBitmap);
         }
     }
 }
